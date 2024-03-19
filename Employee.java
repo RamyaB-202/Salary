c99a37bbdee1c54f4c2a166a4ffcace158bd5b1b
Ramya Bhat <01fe22bca202@kletech.ac.in>
2024-03-19 11:02:25 +0530

    Display Employee Details

---
+++ Employee.java
@@ -0,0 +1,15 @@
+class Employee {
+    public String name="Ram";
+    public int empId=12345;
+    public void Display(){
+        System.out.println("The name of the Employee is "+name);
+        System.out.println("The Emoloyee id of the Employee is "+empId);
+    }
+    public static void main(String[] args){
+        Employee e1 = new Employee();
+        System.out.println("The details of an Employee is");
+        e1.Display();
+        Ex_Employee e2= new Ex_Employee();
+        e2.DisplaySal();
+    }
+}
